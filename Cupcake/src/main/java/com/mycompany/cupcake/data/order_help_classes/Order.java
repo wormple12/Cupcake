94cdbf23afc3a700eace7d5c66e8e5df82243917
unknown <doublenegative@protonmail.com>
2019-03-01 12:01:48 +0100

    hwAdd

---
+++ Cupcake/src/main/java/com/mycompany/cupcake/data/order_help_classes/Order.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cupcake.data.order_help_classes;
+
+/**
+ *
+ * @author Henning
+ */
+public class Order {
+    
+}
