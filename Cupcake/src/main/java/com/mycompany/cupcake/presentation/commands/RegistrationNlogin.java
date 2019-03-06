cb21fca6f5a4df07a77867cd7f3b504fb97bbaa9
Svense <emil.svensmark@gmail.com>
2019-03-06 13:18:57 +0100

    Merge

--- Cupcake/src/main/java/com/mycompany/cupcake/presentation/commands/RegistrationNlogin.java
+++ Cupcake/src/main/java/com/mycompany/cupcake/presentation/commands/RegistrationNlogin.java
@@ -58,22 +58,12 @@ public class RegistrationNlogin extends HttpServlet {
 
             out.println("<h1> Login </h1>");
             out.println("<form action=/Cupcake/RegistrationNlogin method=POST> "
-<<<<<<< HEAD
-                     + "Username: <br> <input type=text name=username> <br> "
                      + "Password: <br> <input type= password name=password> <br> "
                      //+ "Email: <br> <input type= text name=email> <br>"
                      + "<br> <input type=submit>"
                      + "<p><a href=\"c/registration\"> Create New User </a></p>"
                      + "</form>");
-            
-=======
-                    + "Username: <br> <input type=text name=username> <br> "
-                    + "Password: <br> <input type= password name=password> <br> "
-                    //+ "Email: <br> <input type= text name=email> <br>"
-                    + "<br> <input type=submit>"
-                    + "<p><a href=\"c/registration\"> Create New User </a></p>"
-                    + "</form>");
->>>>>>> bbb8dbe6f6bd2e49b17ddf54ca431e4c3383cd84
+
             out.println("</body>");
             out.println("</html>");
 
