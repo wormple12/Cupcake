879c25c5e5c8647d62ca185e5730ff554bd86397
wormple12 <wormple12@hotmail.com>
2019-03-08 12:46:14 +0100

    no success in possibilities

--- Cupcake/src/main/java/com/mycompany/cupcake/presentation/commands/ListCupcakeOptionsCommand.java
+++ Cupcake/src/main/java/com/mycompany/cupcake/presentation/commands/ListCupcakeOptionsCommand.java
@@ -43,12 +43,17 @@ public class ListCupcakeOptionsCommand extends Command {
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
-            out.println("<head>");
-            out.println("<title>WebRecipes: Alle opskrifter</title>");
-            out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">");
-            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css.css\">");
-            out.println("</head>");
+            out.println("<head>\n"
+                    + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+                    + "<title>Cupcake Menu</title>\n"
+                    + "<link rel=\"stylesheet\" type=\"text/css\" href=\"altcss.css\">\n"
+                    + "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                    + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">\n"
+                    + "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>\n"
+                    + "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>\n"
+                    + "</head>");
             out.println("<body>");
+            out.println("<jsp:include page=\"siteheader.jsp\" />");
             out.println("<form action=\"/Cupcake/ProductControl\" method = POST>");
             out.println("<select name = top>Toppings:");
             for (Topping topping : toppings) {
